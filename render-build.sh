24cdea6cd543b84c2c56eefa0d7134f20cc40c4f
K4LM3D <126100750+k4lm3d@users.noreply.github.com>
2026-02-21 13:51:30 +0800

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # exit immediately if a command fails
+
+# Default Hugo version if not set
+HUGO_VERSION=${HUGO_VERSION:-0.155.3}
+
+echo "Using Hugo Extended version: $HUGO_VERSION"
+
+# Download Hugo Extended
+curl -sL https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar -xz
+
+# Run Hugo build
+./hugo --gc --minify
